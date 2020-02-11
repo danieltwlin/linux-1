1b41ead3a14193357f6974e86840a89ce96fe2c3
daniel <daniel.lin.gr@gmail.com>
2020-02-11 13:44:12 +0800

    Update ies_db_update_cn.py

--- db_transfer/ies_db_update_cn.py
+++ db_transfer/ies_db_update_cn.py
@@ -1,6 +1,5 @@
-
 #=== DB Tool ============
-# Date   : 2019.12.17
+# Date   : 2020.2.11
 # Author : daniel
 # Caption: Get new cn db to Alibaba VM
 #========================
@@ -16,23 +15,25 @@ os.chdir('/opt/py')
 
 # Dump DB
 if(1):
-
+        # DB host
+        host ='172.16.2.139'
         if(0):
                 os.system('sudo rm publicitem.sql')
                 os.system('sudo rm answerinfo.sql')
                 os.system('sudo rm learncenter.sql')
 
-        if(0):
-                os.system('/opt/lampp/bin/mysqldump --host=172.16.2.149 publicitem > publicitem.sql')
+        if(1):
+                os.system('/opt/lampp/bin/mysqldump --host=' + host + ' publicitem > publicitem.sql')
 
-                os.system('/opt/lampp/bin/mysqldump --host=172.16.2.149  learncenter answerinfo > answerinfo.sql')
+                os.system('/opt/lampp/bin/mysqldump --host=' + host + '  learncenter answerinfo > answerinfo.sql')
 
-                os.system('/opt/lampp/bin/mysqldump learncenter --host=172.16.2.149 --ignore-table=learncenter.api_a1_log --ignore-table=learncenter.api_log --ignore-table=learncenter.answerinfo > learncenter$
+                os.system('/opt/lampp/bin/mysqldump learncenter --host=' + host + ' --ignore-table=learncenter.api_a1_log --ignore-table=learncenter.api_log --ignore-table=learncenter.answerinfo > learncenter$
 
-                os.system('/opt/lampp/bin/mysqldump --host=172.16.2.149 ies_middle > ies_middle.sql')
+                os.system('/opt/lampp/bin/mysqldump --host=' + host + ' ies_middle > ies_middle.sql')
 
-                os.system('/opt/lampp/bin/mysqldump --host=172.16.2.149 xtrabackup_backupfiles > xtrabackup_backupfiles.sql')
+                os.system('/opt/lampp/bin/mysqldump --host=' + host + ' xtrabackup_backupfiles > xtrabackup_backupfiles.sql')
 
         if(1):
-                os.system('/opt/lampp/bin/mysqldump --host=172.16.2.149 learncenter api_a1_log  > api_a1_log.sql')
-                os.system('/opt/lampp/bin/mysqldump --host=172.16.2.149 learncenter api_log  > api_log.sql')
+                os.system('/opt/lampp/bin/mysqldump --host=' + host + ' learncenter api_a1_log  > api_a1_log.sql')
+                os.system('/opt/lampp/bin/mysqldump --host=' + host + ' learncenter api_log  > api_log.sql')
+
