c1f97c5726fbf39257be3941d80ffecad9a8c196
daniel <daniel.lin.gr@gmail.com>
2020-03-18 14:27:25 +0800

    Update rsync.sh

--- rsync/rsync.sh
+++ rsync/rsync.sh
@@ -9,6 +9,11 @@ rsync -av  pi@192.168.1.12:/mybackup/  /mypath/myfile.gz
 
 # 顯示傳輸進度
 # 如果要讓 rsync 在傳輸檔案時可以即時顯示進度，可以加上 --progress 參數，或是大寫P：
+# -z 會先進行壓縮
+# - h 會用比較好懂的數字表示
 rsync -avzh --progress pi@192.168.1.12:/mypath/myfile.gz /mybackup/
 
 rsync -avP --progress pi@192.168.1.12:/mypath/myfile.gz /mybackup/
+
+
+#-C, --cvs-exclude 排除那些通常不希望傳送的檔案.定義的方式與CVS傳送相同:
