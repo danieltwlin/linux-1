6c2e1e74cfe4b232779f62d37db37e67a3d9a484
daniel <daniel.lin.gr@gmail.com>
2020-03-20 15:17:16 +0800

    Create install_nfs_driver.sh

---
+++ nfs/install_nfs_driver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install nfs-common
+
+
+#On the other hand, if you are trying to mount CIFS, the helper program is provided by cifs-utils. You can install it with:
+# sudo apt install cifs-utils
