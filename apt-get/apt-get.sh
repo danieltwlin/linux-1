99a6f86f6c6e88fd8907672913f57270458830d9
daniel <daniel.lin.gr@gmail.com>
2020-05-05 18:04:10 +0800

    Create apt-get.sh

---
+++ apt-get/apt-get.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#apt-get 安裝指定版本
+#apt-get install <<package name>>=<<version>>
+
+#查询軟體有多少個版本
+#apt-cache madison <<package name>>
+
+#查询軟體有多少個版本 (詳細)
+#apt-cache policy <<package name>>
