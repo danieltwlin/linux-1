0b09be7bafd2f0ec65e536f1461bfb53580685ce
daniel <daniel.lin.gr@gmail.com>
2020-12-28 23:20:18 +0800

    Update scp_db.sh

--- db_transfer/scp_db.sh
+++ db_transfer/scp_db.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-sshpass -p'pwd' scp rd@host:/opt/py/publicitem.sql .
-sshpass -p'pwd' scp rd@host:/opt/py/learncenter.sql .
-sshpass -p'pwd' scp rd@host:/opt/py/answerinfo.sql .
-sshpass -p'pwd' scp rd@host:/opt/py/xtrabackup_backupfiles.sql .
-sshpass -p'pwd' scp rd@host:/opt/py/ies_middle.sql  .
-sshpass -p'pwd' scp rd@host:/opt/py/api_a1_log.sql  .
-sshpass -p'pwd' scp rd@host:/opt/py/api_log.sql  .
+sshpass -p'pwd' scp rd@host:/opt/py/db2.sql .
+sshpass -p'pwd' scp rd@host:/opt/py/db1.sql .
+sshpass -p'pwd' scp rd@host:/opt/py/tb1.sql .
+sshpass -p'pwd' scp rd@host:/opt/py/tb2.sql .
+sshpass -p'pwd' scp rd@host:/opt/py/tb3.sql  .
+sshpass -p'pwd' scp rd@host:/opt/py/tb3.sql  .
+sshpass -p'pwd' scp rd@host:/opt/py/tb5.sql  .
 
 # sample 
 host=192.168.xx.xx
 pwd=123
-/usr/bin/sshpass -p$pwd /opt/lampp/bin/mysqldump -uroot -p --opt -a --host=$host publicitem > publicitem.sql
+/usr/bin/sshpass -p$pwd /opt/lampp/bin/mysqldump -uroot -p --opt -a --host=$host db2 > db2.sql
 
 
 # copy to container
-#docker cp publicitem.sql container_id:/opt
-#docker cp learncenter.sql container_id:/opt
-#docker cp answerinfo.sql container_id:/opt
-#docker cp ies_middle.sql container_id:/opt
-#docker cp xtrabackup_backupfiles.sql container_id:/opt
-#docker cp api_a1_log.sql container_id:/opt
-#docker cp api_log.sql container_id:/opt
+#docker cp db2.sql container_id:/opt
+#docker cp db1.sql container_id:/opt
+#docker cp tb1.sql container_id:/opt
+#docker cp tb2.sql container_id:/opt
+#docker cp tb3.sql container_id:/opt
+#docker cp tb4.sql container_id:/opt
+#docker cp tb5.sql container_id:/opt
 
 # docker exec -it  laradock_mysql_1 /bin/bash
